8b16fea0254e0fd6aaf676c420bb5e62746679a6
saif1337 <saif71@hotmail.com>
2015-05-07 17:27:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"   "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+
+
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+	<title>Bahamatest</title>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	<!--link rel="stylesheet" type="text/css" href="http://www.ukgn.webartistuk.com/tigers/mystyle.css">
+
+    <!-- BootstrapValidator JS -->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js"></script>
+  <!-- BootstrapValidator CSS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/jquery.bootstrapvalidator/0.5.3/css/bootstrapValidator.min.css"/>
+
+
+    <!--link rel="stylesheet" href="http://ukgn.webartistuk.com/tigers/jsLibrary/bootstrapwisihtml5/bootstrap3-wysihtml5.min.css"/>
+    <script src="http://ukgn.webartistuk.com/tigers/jsLibrary/bootstrapwisihtml5/bootstrap3-wysihtml5.all.min.js"></script-->
+
+
+
+
+<style>
+
+</style>
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12" >
+				<div class="panel panel-default" ><!--panel-->
+					<div class="col-md-12" style="padding: 0px 0px 0px 0px;">
+						<div  style=" float: left; background:url(img/banner.png);background-size:cover; ">
+							<div class="col-md-12" >
+			              	<div class="col-md-8" >
+								
+							</div>
+							<div class="col-md-4" >
+								<div class="col-md-12" style="  padding: 20px 0px 0px 20px;">
+								<div class="panel panel-default" style=" margin: 0px; padding: 0px; border-radius: 10px 10px 0 0;"><!--panel-->
+									<div class="panel-body" style="padding: 0px; margin: 20px 20px 20px 20px; ">
+										<div class="col-md-12" style="padding: 0px ">
+											<h3 style="margin: 0px 0px 20px 0px; color: #66c1cb">Featured video</h3>
+			                				<a href"#" class="iconClick" id="yLUxzgdeD78" data-toggle="modal" data-target="#newTeamprofile">
+			                					<img style="background:url(http://img.youtube.com/vi/yLUxzgdeD78/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+			                				</a>
+			                			</div>
+			                			<div class="col-md-12" style="padding: 0px ">
+			                				<h3 style="color: #66c1cb">Kayaking</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12" style="padding: 0px ">
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed.  </p>
+			                			</div>
+									</div>
+								</div>
+								</div>
+							</div>
+							</div>
+						</div>
+            			
+						</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12" >
+				<div class="panel panel-default"  style=" border: 0px;"><!--panel-->
+					<div class="col-md-12"   style="background-color:#e0f5f6; margin: 0px 0px 40px 0px;">
+						<div>
+			              	<div class="col-md-8" >
+								
+									<div class="panel panel-default" style=" background-color:#e0f5f6; padding: 0px; height: 20px; margin: 0px 0px 0px 0px; border: 0px solid black;"><!--panel-->
+									</div>
+									<div  style=" background-color:#e0f5f6; padding: 0px; height: 40px; margin: 0px 0px 00px 0px; border: 0px solid #e0f5f6;"><!--panel-->
+										<div class="col-md-4"  style=" background-color:#e0f5f6; padding: 0 20px 30px 0; height: 40px; margin: 0px 0px 0px 0px; border: 0px solid #e0f5f6;">
+											<div class="panel panel-default" style="border: 0px; padding: 25px; background-color:white; height: 65px; border-radius: 10px 10px 0px 0px; margin: 0px 0px 0px 0px;"><!--panel-->
+												<center><a style="color:#66c1cb; font-size: 20px ">Latest Media</a></center>
+											</div>
+										</div>
+										<div class="col-md-4" >
+											<div class="panel panel-default" style="border: 0px; padding: 25px; background-color:#e0f5f6; height: 65px; border-radius: 10px 10px 0px 0px; margin: 0px 0px 0px 0px;"><!--panel-->
+												<center><a style="color:#66c1cb; font-size: 20px ">Video Gallery</a></center>
+											</div>
+										</div>
+										<div class="col-md-4" >
+											<div class="panel panel-default" style="border: 0px; padding: 25px; background-color:#e0f5f6; height: 65px; border-radius: 10px 10px 0px 0px; margin: 0px 0px 0px 0px;"><!--panel-->
+												<center><a style="color:#66c1cb; font-size: 20px ">Photo Gallery</a></center>
+											</div>
+										</div>
+									</div>
+									<!-- 	<div style="float:left; height:30px; border: 1px solid black; width: 100%">
+										</div>
+										<div style="float:left;  border: 1px solid red; height: 50px; width: 100%">
+											<div class="col-md-3"  style="background-color:black; " >
+												
+											</div>
+											<div class="col-md-3"  style="background-color:black;" >
+											</div>
+											<div class="col-md-3"  style="background-color:black; " >
+											</div>
+										</div> -->
+								
+							</div>
+							<div class="col-md-4" >
+								<div class="col-md-12" style="  padding: 0px 0px 0px 20px;">
+									<div class="panel panel-default" style=" padding: 0px; background-color:#66c1cb; border-radius: 0px 0px 10px 10px;"><!--panel-->
+										<div class="panel-body" style="padding: 0px;  ">
+											<div class="col-md-12" style="padding: 0px;  ">
+												<div class="col-md-2" style="padding: 10px;  ">
+													<img src="img/playBut.png" width = 100%>
+												</div>
+												<div class="col-md-8" style="padding: 0px 0px 0px 10px;  ">
+												<h4 style="margin: 15px 0px 20px 25px; color: white">Featured video</h4>
+				                				</div>
+				                				<div class="col-md-2" style="padding: 20px;  ">
+				                					<img src="img/arrow.png" width = 80%>
+				                				</div>
+				                			</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+            			
+					</div>
+					
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12" >
+				<div class="panel panel-default" style=" border: 0px;"><!--panel-->
+                	
+            			<div class="col-md-8" >
+            				<div class="col-md-4" style="padding: 0px 0px 0px 0px;">
+	            				<div class="panel panel-default" style=" border: 0px;"><!--panel-->
+		                			<div class="panel-body" style="padding: 0px ">
+		                				<div class="col-md-12" >
+			                				<a href"#" class="iconClick" id="yLUxzgdeD78" data-toggle="modal" data-target="#newTeamprofile">
+			                					<img style="background:url(http://img.youtube.com/vi/yLUxzgdeD78/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+			                				</a>
+			                			</div>
+			                			<div class="col-md-12">
+			                				<h3 style="color: #66c1cb">Kayaking</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12">
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed.  </p>
+			                			</div>
+
+		                			</div>
+	                			</div> 
+            				</div>
+            				<div class="col-md-4" style="padding: 0px 0px 0px 0px;">
+	            				<div class="panel panel-default"  style=" border: 0px;"><!--panel-->
+		                			<div class="panel-body" style="padding: 0px ">
+		                				<div class="col-md-12" >
+	                					<a href"#" class="iconClick" id="bNucJgetMjE" data-toggle="modal" data-target="#newTeamprofile">
+	                						<img style="background:url(http://img.youtube.com/vi/bNucJgetMjE/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+	                					</a>
+	                					</div>
+			                			<div class="col-md-12" >
+			                				<h3 style="color: #66c1cb">Diving</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12" >
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed. </p>
+			                			</div>
+		                			</div>
+	                			</div> 
+            				</div>
+            				<div class="col-md-4" style="padding: 0px 0px 0px 0px;">
+	            				<div class="panel panel-default" style=" border: 0px;"><!--panel-->
+		                			<div class="panel-body" style="padding: 0px ">
+		                				<div class="col-md-12" >
+		                				<a href"#" class="iconClick" id="E1h1-dse8Lo" data-toggle="modal" data-target="#newTeamprofile">
+		                					<img style="background:url(http://img.youtube.com/vi/E1h1-dse8Lo/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+		                				</a>
+		                				</div>
+			                			<div class="col-md-12">
+			                				<h3 style="color: #66c1cb">Wildlife</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12">
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed. </p>
+			                			</div>		
+		                			</div>
+	                			</div> 
+            				</div>
+            			</div>
+            			<div class="col-md-4" >
+            				<div class="col-md-12" >
+            				<div class="panel panel-default" style="border: 0px;"><!--panel-->
+	                			<div class="panel-body" style="padding: 0px ">
+	                				<img src="img/poster1.jpg" width=100%>
+	                			</div>
+                			</div> 
+                			</div>  
+            			</div>
+					
+                </div>   
+
+				<div class="panel panel-default" style=" border: 0px;"><!--panel-->
+                	
+            			<div class="col-md-8" >
+            				<div class="col-md-4" style="padding: 0px 0px 0px 0px;">
+	            				<div class="panel panel-default" style=" border: 0px;"><!--panel-->
+		                			<div class="panel-body" style="padding: 0px ">
+		                				<div class="col-md-12" >
+			                				<a href"#" class="iconClick" id="yLUxzgdeD78" data-toggle="modal" data-target="#newTeamprofile">
+			                					<img style="background:url(http://img.youtube.com/vi/yLUxzgdeD78/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+			                				</a>
+			                			</div>
+			                			<div class="col-md-12">
+			                				<h3 style="color: #66c1cb">Kayaking</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12">
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed.  </p>
+			                			</div>
+
+		                			</div>
+	                			</div> 
+            				</div>
+            				<div class="col-md-4" style="padding: 0px 0px 0px 0px;">
+	            				<div class="panel panel-default"  style=" border: 0px;"><!--panel-->
+		                			<div class="panel-body" style="padding: 0px ">
+		                				<div class="col-md-12" >
+	                					<a href"#" class="iconClick" id="bNucJgetMjE" data-toggle="modal" data-target="#newTeamprofile">
+	                						<img style="background:url(http://img.youtube.com/vi/bNucJgetMjE/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+	                					</a>
+	                					</div>
+			                			<div class="col-md-12" >
+			                				<h3 style="color: #66c1cb">Diving</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12" >
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed. </p>
+			                			</div>
+		                			</div>
+	                			</div> 
+            				</div>
+            				<div class="col-md-4" style="padding: 0px 0px 0px 0px;">
+	            				<div class="panel panel-default" style=" border: 0px;"><!--panel-->
+		                			<div class="panel-body" style="padding: 0px ">
+		                				<div class="col-md-12" >
+		                				<a href"#" class="iconClick" id="E1h1-dse8Lo" data-toggle="modal" data-target="#newTeamprofile">
+		                					<img style="background:url(http://img.youtube.com/vi/E1h1-dse8Lo/0.jpg); background-size:cover" src="img/white-play-button-png.png" width=100%>
+		                				</a>
+		                				</div>
+			                			<div class="col-md-12">
+			                				<h3 style="color: #66c1cb">Wildlife</h3>
+			                				<div style="border-bottom: 3px solid #66c1cb; margin: 15px 0px 15px 0px;"></div>
+			                			</div>
+			                			<div class="col-md-12">
+			                				<p>The Bahamas is a coral-based archipelago in the Atlantic Ocean, comprising 700 islands and cays that range from uninhabited to resort-packed. </p>
+			                			</div>		
+		                			</div>
+	                			</div> 
+            				</div>
+            			</div>
+            			<div class="col-md-4" >
+            				<div class="col-md-12" >
+            				<div class="panel panel-default" style="border: 0px;"><!--panel-->
+	                			<div class="panel-body" style="padding: 0px ">
+	                				<img src="img/poster2.jpg" width=100%>
+	                			</div>
+                			</div>  
+                			</div>  
+            			</div>
+					
+                </div> 
+
+			</div>
+		</div>
+	</div>
+
+
+
+<div class="modal fade" id="newTeamprofile" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Video</h4>
+      </div>
+      <div class="modal-body">
+	
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
+<script type='text/javascript'>
+$('.iconClick').on('click', function (e) {
+
+
+	var idvalue = $(this).attr("id");
+	var iframe = "<iframe id='player' type='text/html' width='100%' height='390' src='http://www.youtube.com/embed/"+idvalue+"?enablejsapi=1' frameborder='0'></iframe>";
+	$(".modal-body").html(iframe);
+
+
+	});
+</script>
